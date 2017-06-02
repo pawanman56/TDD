6cb4576eb33d57f0236afdd1b3a865089abff718
Pawan Sunuwar <pawanman56@gmail.com>
2017-06-02 16:39:43 +0545

    Test create action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new post" do
+        expect {
+          post :create, post: FactoryGirl.attributes_for(:post)
+        }.to change(Post, :count).by(1)
+      end
+      it "redirects to root_path" do 
+      
+      end
+    end
+
+    context "with invalid attributes" do
+      it "doesn't create a new post" do
+
+      end
+      it "renders the new template" do
+
+      end
+    end
+  end
+  
 end
