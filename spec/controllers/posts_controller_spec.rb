0282fae448416ba6bfbec0eb14fc1023c488e04e
Pawan Sunuwar <pawanman56@gmail.com>
2017-06-02 16:31:55 +0545

    Test new action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,4 +17,11 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
 end
