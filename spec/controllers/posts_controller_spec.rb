52a64719bc2741b5b8d7e47f3d4586725b609c97
Pawan Sunuwar <pawanman56@gmail.com>
2017-06-02 21:09:24 +0545

    Test update action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -49,4 +49,12 @@ RSpec.describe PostsController, type: :controller do
     end
   end
   
+  describe "GET #edit" do
+    it "renders the edit template" do
+      post = FactoryGirl.create(:post)
+      get :edit, id: post.id
+      expect(response).to render_template :edit
+    end
+  end
+  
 end
