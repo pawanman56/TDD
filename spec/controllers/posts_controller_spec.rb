eb53819ba63f76a9d653eb5b82b4cc2e2dab73bf
Pawan Sunuwar <pawanman56@gmail.com>
2017-06-03 13:00:05 +0545

    Test update action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -57,4 +57,36 @@ RSpec.describe PostsController, type: :controller do
     end
   end
   
+  describe "PUT #update" do
+    context "when attributes are valid" do
+      it "updates the post" do
+        post = FactoryGirl.create(:post)
+        put :update, id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry')
+        post.reload
+        expect(post.title).to eq('New Title')
+        expect(post.author).to eq('Larry')
+      end
+      it "redirects to root_path" do
+        post = FactoryGirl.create(:post)
+        put :update, id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry')
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "when attributes are invalid" do
+      it "doesn't updates the posts" do
+        post = FactoryGirl.create(:post)
+        put :update, id: post.id, post: FactoryGirl.attributes_for(:post, title: 'New Title', author: 'Larry', content: 'Hi')
+        post.reload
+        expect(post.title).to_not eq('New Title')
+        expect(post.author).to_not eq('Larry')
+        expect(post.content).to_not eq('Hi')
+      end
+      it "renders the edit template" do
+        post = FactoryGirl.create(:post)
+        put :update, id: post.id, post: FactoryGirl.attributes_for(:invalid_post)
+        expect(response).to render_template :edit
+      end
+    end
+  end
+  
 end
